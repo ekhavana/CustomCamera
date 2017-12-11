d152bb179c024ac3a543d55ccadfe2eeebb7d01c
Evgeny Khavana <ekhavana@havanadev.us>
2017-12-10 20:51:49 -0500

    App Update

---
+++ CustomCamera/PreviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PreviewViewController.swift
+//  CustomCamera
+//
+//  Created by Evgeny on 12/10/17.
+//  Copyright © 2017 HavanaDev. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func cancelButton_TouchUpInside(_ sender: Any) {
+    }
+    
+    @IBAction func saveButton_TouchUpInside(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
